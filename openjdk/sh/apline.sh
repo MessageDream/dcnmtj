072a3cf98f6794f25b47a2b2b8ba77ec0d337b98
liyuzhao <8603583yuPENG>
2020-07-28 15:38:48 +0800

    直接使用wget拉取文件

--- openjdk/sh/apline.sh
+++ openjdk/sh/apline.sh
@@ -15,7 +15,7 @@ fi
 
 if [ "${MAVEN_VERSION}" != "" ]; then
   echo "---------- Install MAVEN ${MAVEN_VERSION}----------"
-  ls -lh /tmp/extensions/${MAVEN_VERSION}
+  cd /tmp/extensions && wget https://mirrors.cnnic.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz --no-check-certificate
   tar -zxvf /tmp/extensions/${MAVEN_VERSION}
   mv apache-maven-3.6.3 /usr/local/maven
   ln -s /usr/local/maven/bin/mvn  /usr/bin/mvn
